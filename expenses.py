822782378edef5b4096974e07e1cabc12ccb47fe
Damian Lujan <damianlujan9@icloud.com>
2026-04-10 23:46:44 +0000

    Expenses

---
+++ expenses.py
@@ -0,0 +1,5 @@
+food = float(input("Enter the amount you've spent on food this month: "))
+gas = float(input("Enter the amount you've spent on gas this month: "))
+bills = float(input("Enter the amount you've spent on bills this month: "))
+total_expenses = food + gas + bills 
+print("Your total expenses for the month are: $", total_expenses)
